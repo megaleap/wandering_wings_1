269204e273b2591a30a335c3b321721953e43d96
Ushasri Gajal <ushasri.sanganapalli@saasysavant.com>
2025-12-18 11:25:47 +0530

    Initial commit from Create Next App

---
+++ src/app/layout.js
@@ -0,0 +1,13 @@
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
